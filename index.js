d6f34a6e77f20e6a55d55fc83d5e3f803f44e025
tbohn2 <tanner.bohn@yahoo.com>
2023-04-19 12:10:12 -0700

    Added to ToC

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ function readMe(title, description, instructions, usage, license, contributing, 
     <li><a href="#description">Description</a></li>
     <li><a href="#installation">Installation</a></li>
     <li><a href="#usage">Usage</a></li>
-    <li><a href="#test">Roadmap</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contributing">Contribution</a></li>
+    <li><a href="#test">Test</a></li>
     <li><a href="#questions">Questions</a></li>
   </ol>
 </details>
