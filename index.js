9205246011b34c817782255926e9383815d3c192
tbohn2 <tanner.bohn@yahoo.com>
2023-04-19 11:54:31 -0700

    Added README template literal and email/linkedin

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+function readMe(title, description, instructions, usage, test, github, linkedin, email) {
+    `
+# ${title}
+
+## Description
+
+${description}
+
+## Installation instructions
+
+${instructions}
+
+## Usage
+
+${usage}
+
+## Test
+
+${test}
+
+## Questions
+
+Feel free to contact the developer with any further questions:
+    LinkedIn: ${linkedin}
+    Email: ${email}
+
+The application repository found at [${github}]${github}
+`
+}
+
 inquirer
     .prompt([
         {
@@ -30,7 +60,21 @@ inquirer
         },
         {
             type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub URL.',
+        },
+        {
+            type: 'input',
             name: 'linkedin',
             message: 'Enter your LinkedIn URL.',
         },
-    ]) 
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address.',
+        },
+    ])
+    .then((answers) => {
+        console.log(answers);
+    }
+    )
